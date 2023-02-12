5b153c1f08cca9e2c5a884b79f420d7db747f9df
joieng1 <johncieng1@gmail.com>
2023-02-11 18:37:41 -0800

    fix: missing import

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 // Font Awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faPhoneSquare } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 
